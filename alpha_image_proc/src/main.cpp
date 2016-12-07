98aae3bf1b882471d135f0597918f94405b79145
yycho0108 <jchocholate@gmail.com>
2016-12-07 01:00:32 -0500

    working cone isolation

--- alpha_image_proc/src/main.cpp
+++ alpha_image_proc/src/main.cpp
@@ -1,34 +1,75 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-using namespace cv;
+int lh=4, hh=7; // reasonable default values
 
-class Detector{
-	SimpleBlobDetector detector;
+// kernel
+cv::Mat ker = cv::getStructuringElement(cv::MORPH_ERODE, cv::Size(3,3), cv::Point(1,1));
 
+class ImageConverter
+{
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+
+	public:
+	ImageConverter()
+		: it_(nh_)
+	{
+		// Subscrive to input video feed and publish output video feed
+		image_sub_ = it_.subscribe("/image_raw", 1, &ImageConverter::imageCb, this);
+	}
+
+	~ImageConverter()
+	{
+
+	}
+
+	void imageCb(const sensor_msgs::ImageConstPtr& msg)
+	{
+		cv_bridge::CvImagePtr cv_ptr;
+		try
+		{
+			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		}
+		catch (cv_bridge::Exception& e)
+		{
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+
+		// Draw an example circle on the video stream
+		cv::Mat thresh;
+		cv::GaussianBlur(cv_ptr->image, cv_ptr->image,cv::Size(13,13), 0, 0);
+		cv::cvtColor(cv_ptr->image, cv_ptr->image, cv::COLOR_BGR2HSV);
+		cv::inRange(cv_ptr->image, cv::Scalar(lh,0,0), cv::Scalar(hh,255,255), thresh);
+		cv::erode(thresh, thresh, ker);
+
+		// Update GUI Window
+		cv::imshow("img", cv_ptr->image);
+		cv::imshow("thresh", thresh);
+	}
 };
 
-void proc(){
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "image_converter");
+	ImageConverter ic;
+	cv::namedWindow("ctrl");
 
-}
+	cv::createTrackbar("lh", "ctrl", &lh, 179);
+	cv::createTrackbar("hh", "ctrl", &hh, 179);
 
-int main(){
-	//cv2::inRange(
-	// Read image
-	Mat im = imread( "blob.jpg", IMREAD_GRAYSCALE );
-	// Set up the detector with default parameters.
-	SimpleBlobDetector detector;
+	while(ros::ok()){
+		ros::spinOnce();
+		cv::waitKey(3);
+	}
 
-	// Detect blobs.
-	std::vector<KeyPoint> keypoints;
-	detector.detect( im, keypoints);
-
-	// Draw detected blobs as red circles.
-	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
-	Mat im_with_keypoints;
-	drawKeypoints( im, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-
-	// Show blobs
-	imshow("keypoints", im_with_keypoints );
-	waitKey(0);
+	return 0;
 }
