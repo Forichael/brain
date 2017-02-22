f680aab94f9b810a79ae12f7d57d599252f62a8e
Eric Miller <eric@legoaces.org>
2017-02-21 21:26:28 -0500

    Removed sketchiness and added left wheel encoder handling

--- arduino code/main/encoders.h
+++ arduino code/main/encoders.h
@@ -9,39 +9,33 @@
 #define MAIN_ODOMETRY_H
 
 // Setup encoder data
-Encoder rightEncoder(18, 19);
+Encoder rightEncoder(18, 20);
+Encoder leftEncoder(19, 21);
 
 const int ODOM_LOOP_TIME = 20; // ms
 
 std_msgs::Int16 right_encoder_msg;
 ros::Publisher right_encoder_pub("/rwheel", &right_encoder_msg);
 
+std_msgs::Int16 left_encoder_msg;
+ros::Publisher left_encoder_pub("/lwheel", &left_encoder_msg);
 
-int hackEncoderVal = 0;
 
 void updateEncoders(){
-    right_encoder_msg.data = hackEncoderVal;
-    //right_encoder_msg.data = rightEncoder.read();
+    right_encoder_msg.data = rightEncoder.read();
     right_encoder_pub.publish( &right_encoder_msg );
+
+    left_encoder_msg.data = leftEncoder.read();
+    left_encoder_pub.publish( &left_encoder_msg );
 }
 
 void setupEncoders(){
     nh.advertise(right_encoder_pub);
+    nh.advertise(left_encoder_pub);
 }
 
 void loopEncoders(float right_dir){
     static unsigned long lastOdomTime = millis();
-    static int lastEncoderValue = rightEncoder.read();
-
-    int value = rightEncoder.read();
-    if (lastEncoderValue != value){
-        if (right_dir > 0)
-            hackEncoderVal += abs(value - lastEncoderValue);
-        else
-            hackEncoderVal -= abs(value - lastEncoderValue);
-
-        lastEncoderValue = value;
-    }
 
     if (millis() - lastOdomTime >= ODOM_LOOP_TIME) {
         lastOdomTime = millis();
