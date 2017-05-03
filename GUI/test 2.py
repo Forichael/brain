fb260df115d1bc92b05df8073e907f90cb0f2afa
lee <leehiaohuang@gmail.com>
2017-05-02 20:51:36 -0400

    Updates on trajectory

--- GUI/test 2.py
+++ GUI/test 2.py
@@ -29,65 +29,83 @@ class Page1(Page): #Localization - Position
         self.newlabel = tk.Label(self,text="")
         self.CV = tk.Canvas(self, height=500, width=700, borderwidth=0, highlightthickness=0)
         # rospy.Subscriber(.....)
+        self.trajectory = []
         rospy.Subscriber("/odometry/filtered/local", Odometry, self.Pos)
-        
+
+        #drawgrid
+        for a in range(15):
+            self.CV.create_line(50*a, 0, 50*a,500  ) #vertical line
+            self.CV.create_line(0, 50*a, 700, 50*a) #horizontal line
+            self.CV.pack(side="left", fill = "both",expand =True)
+            a=a+1
     
    def Pos(self, msg): 
         self.newlabel.pack_forget()
         self.newlabel = tk.Label(self, text= str(msg.pose.pose), anchor=W)
         self.newlabel.pack(side="right", fill="both", expand=True)
+        i1 = msg.pose.pose.position.x
+        j1 = msg.pose.pose.position.y
 
-        #drawgrid
-        a=0      
-            
-        while (a<15):
-            self.CV.create_line(50*a, 0, 50*a,500  ) #vertical line
-            self.CV.create_line(0, 50*a, 700, 50*a) #horizontal line
-            self.CV.pack(side="left", fill = "both",expand =True)
-            a=a+1
-
-        #draw direction
-        i=350
-        j=0
-        iold=0
-        jold=0
-        ishift=0
-        jshift=0
-        new = 0
-        old = 0
+        self.trajectory.append((i1,j1))
         
-        while(i>0, i<700, j>0, j<500):
+        if len(self.trajectory) > 1:
             
-            ishiftold=ishift
-            jshiftold=jshift
-            print (iold,jold,i+ishiftold,j+jshiftold)            
-
-            i1 = msg.pose.pose.position.x
-            j1 = msg.pose.pose.position.y
-            time.sleep(7)
-            i2 =msg.pose.pose.position.x
-            j2 = msg.pose.pose.position.y                       
-            print (iold,jold,i+ishiftold,j+jshiftold, i1,i2, j1,j2)
-
-            if (new>old):               
-                    self.CV.create_line(iold, jold, iold+ishiftold, jold+jshiftold, fill="red", width=2)
-                    
-                    #produce new starting point
-                    i=i+ishift
-                    j=j+jshift
-                    print(old, i, j)
-                        
-            iold=i
-            jold=j
-            #ishift = input ('ishift=')
-            ishift = i2-i1
-            #jshift = input('jshift=')
-            jshift = j2-j1
-            new=new+1
-            self.CV.create_line(i, j, i+ishift, j+jshift, fill="green2", width=2)
-            print(i , j, i+ishift, j+jshift)
-            time.sleep(2)
-            
+            # DRAW ALL THE OLD TRAJECTORY
+            for src,dst in zip(self.trajectory[:-1], self.trajectory[1:]):
+                srci, srcj = src
+                dsti, dstj = dst
+                self.CV.create_line(srci, srcj, dsti, dstj, fill="red", width=2)
+                
+            # OVERDRAW NEWEST WITH DIFFERENT COLOR
+            srci, srcj = self.trajectory[-2]
+            dsti, dstj = self.trajectory[-1]
+            self.CV.create_line(srci, srcj, dsti, dstj, fill="green2", width=2)
+     
+##                    
+##           
+##        #draw direction
+##        i=350
+##        j=0
+##        iold=0
+##        jold=0
+##        ishift=0
+##        jshift=0
+##        new = 0
+##        old = 0
+##
+##        prv_i = 0
+##        prv_j = 0
+##        
+##        while(i>0, i<700, j>0, j<500):
+##            
+##            ishiftold=ishift
+##            jshiftold=jshift
+##            print (iold,jold,i+ishiftold,j+jshiftold)            
+##            
+##            time.sleep(7)
+##            i2 =msg.pose.pose.position.x
+##            j2 = msg.pose.pose.position.y                       
+##            print (iold,jold,i+ishiftold,j+jshiftold, i1,i2, j1,j2)
+##            
+##            if (new>old):               
+##                    self.CV.create_line(iold, jold, iold+ishiftold, jold+jshiftold, fill="red", width=2)
+##                    
+##                    #produce new starting point
+##                    i=i+ishift
+##                    j=j+jshift
+##                    print(old, i, j)
+##                        
+##            iold=i
+##            jold=j
+##            #ishift = input ('ishift=')#
+##            ishift = i2-i1
+##            #jshift = input('jshift=')#
+##            jshift = j2-j1
+##            new=new+1
+##            self.CV.create_line(i, j, i+ishift, j+jshift, fill="green2", width=2)
+##            print(i , j, i+ishift, j+jshift)
+##            time.sleep(2)
+##            
         
 class Page2(Page): #USB hub - IMU 
     
